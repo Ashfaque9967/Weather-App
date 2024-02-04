52406e5a506c74699c2bca2de6215266501a1457
Ashfaque <shaikhashfaque220@gmail.com>
2024-02-04 22:18:34 +0530

    File structure created

---
+++ src/WeatherApp.java
@@ -0,0 +1,34 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+// retreive weather data from API - this backend logic will fetch the latest weather
+// data from the external API and return it. The GUI will
+// display this data to the user
+public class WeatherApp {
+
+    // getWeatherData() -> ashfaque
+
+    // getLocationData() -> ashhab
+
+    // fetchApiResponse() -> atharva
+
+    // findIndexOfCurrentTime() -> umer
+
+    // getCurrentTime() -> sahil
+
+    // convertWeatherCode() -> ashfaque
+}
+
+
+
+
+
+
