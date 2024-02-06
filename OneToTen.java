599ce9acc46e6bf48b85710cc4e4594ee2f2ba3a
MuhammadAshhab-23 <144796970+MuhammadAshhab-23@users.noreply.github.com>
2024-02-07 01:30:08 +0530

    Done Test Question

---
+++ OneToTen.java
@@ -0,0 +1,16 @@
+package M_Ashhab;
+
+public class OneToTen
+{
+	public static void main(String[] args)
+	{
+		//print the result
+		System.out.println("Output is : ");
+
+		//loop  to print 1 to 10.
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
