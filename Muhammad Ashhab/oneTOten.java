14069d48cb1b8c3e21317efd2f4e62ba13561dfd
Ashfaque <shaikhashfaque220@gmail.com>
2024-02-12 07:24:18 +0530

    checked the test by ashhab & added getWeatherData()

--- Muhammad Ashhab/oneTOten.java
+++
@@ -1,8 +0,0 @@
-public class oneTOten {
-    public static void main(String[] args) {
-        // Loop from 1 to 10
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
-    }
-}
