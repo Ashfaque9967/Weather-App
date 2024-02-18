66ccfbf8fb6f816e955e6fcc189b994c1d922578
MuhammadAshhab-23 <144796970+MuhammadAshhab-23@users.noreply.github.com>
2024-02-18 20:49:32 +0530

    WheatherAppGui

--- src/WeatherAppGui.java
+++ src/WeatherAppGui.java
@@ -12,6 +12,27 @@ import java.io.IOException;
 public class WeatherAppGui extends JFrame {
 
     // WeatherAppGui() -> Ashhab
+    public WeatherAppGui(){
+        // setup our gui and add a title
+        super("Weather App");
+
+        // configure gui to end the program's process once it has been closed
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // set the size of our gui (in pixels)
+        setSize(450, 650);
+
+        // load our gui at the center of the screen
+        setLocationRelativeTo(null);
+
+        // make our layout manager null to manually position our components within the gui
+        setLayout(null);
+
+        // prevent any resize of our gui
+        setResizable(false);
+
+      //  addGuiComponents(); <--- ATHARV UNCOMMENT THIS LINE AFTER YOU WRITE YOUR CODE
+    }
 
     // addGuiComponents() -> atharva
 
