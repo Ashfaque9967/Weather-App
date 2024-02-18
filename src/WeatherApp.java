9f693f999079e3c86a4c2dd1d929bc9a7307f72a
MuhammadAshhab-23 <144796970+MuhammadAshhab-23@users.noreply.github.com>
2024-02-18 20:39:09 +0530

    get location code

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -74,6 +74,55 @@ public class WeatherApp {
 //    }
 
     // getLocationData() -> ashhab
+    public static JSONArray getLocationData(String locationName){
+        // replace any whitespace in location name to + to adhere to API's request format
+        locationName = locationName.replaceAll(" ", "+");
+
+        // build API url with location parameter
+        String urlString = "https://geocoding-api.open-meteo.com/v1/search?name=" +
+                locationName + "&count=10&language=en&format=json";
+
+        try{
+            // call api and get a response
+            HttpURLConnection conn = fetchApiResponse(urlString);
+
+            // check response status
+            // 200 means successful connection
+            if(conn.getResponseCode() != 200){
+                System.out.println("Error: Could not connect to API");
+                return null;
+            }else{
+                // store the API results
+                StringBuilder resultJson = new StringBuilder();
+                Scanner scanner = new Scanner(conn.getInputStream());
+
+                // read and store the resulting json data into our string builder
+                while(scanner.hasNext()){
+                    resultJson.append(scanner.nextLine());
+                }
+
+                // close scanner
+                scanner.close();
+
+                // close url connection
+                conn.disconnect();
+
+                // parse the JSON string into a JSON obj
+                JSONParser parser = new JSONParser();
+                JSONObject resultsJsonObj = (JSONObject) parser.parse(String.valueOf(resultJson));
+
+                // get the list of location data the API gtenerated from the lcoation name
+                JSONArray locationData = (JSONArray) resultsJsonObj.get("results");
+                return locationData;
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        // couldn't find location
+        return null;
+    }
 
     private static HttpURLConnection fetchApiResponse(String urlString){
         try{
